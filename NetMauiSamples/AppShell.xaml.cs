5ece620fefd0751771f483cbfd9166518f7d3523
Rafael Osipov <rafael.osipov@outlook.com>
2025-02-22 18:54:26 +0300

    Initial commit

---
+++ NetMauiSamples/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NetMauiSamples
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NetMauiSamples/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NetMauiSamples
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
